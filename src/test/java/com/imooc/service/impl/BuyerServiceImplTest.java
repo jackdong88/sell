35feaccb354cc06fdbcd37d31142112a35df5f8c
jackdong88 <2791900090@qq.com>
2019-08-06 00:23:11 +0800

    初始化 commit

---
+++ src/test/java/com/imooc/service/impl/BuyerServiceImplTest.java
@@ -0,0 +1,12 @@
+package com.imooc.service.impl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BuyerServiceImplTest {
+
+    @Test
+    public void findOrderOne() {
+    }
+}
